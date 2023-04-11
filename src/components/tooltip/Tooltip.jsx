57eec41d6d3ca2c712ab7f2ad95e9abbe5a0c869
Abhishek Katore <katoreabhi27@gmail.com>
2023-04-11 13:59:45 +0530

    updated CSS, HTML and JSX file

--- src/components/tooltip/Tooltip.jsx
+++ src/components/tooltip/Tooltip.jsx
@@ -3,14 +3,17 @@ import "./tooltip.css"
 
 
 export const Tooltip = () => {
-
+    //state to set position
     const [position, setPosition] = useState("");
+    //state to set visibility of tooltip
     const [displayTip, setDisplayTip] = useState(false)
+    //function to handle hoverOver
     const hoverIn = (e) => {
         console.log(e.target.value)
         setPosition(e.target.value)
         setDisplayTip(true);
     }
+    //function to handle hoverOut
     const hoverOut = () => {
         setDisplayTip(false);
     }
@@ -21,8 +24,9 @@ export const Tooltip = () => {
             <button onMouseOver={hoverIn} onMouseOut={hoverOut} value="bottom" className="btn">Bottom</button>
             <button onMouseOver={hoverIn} onMouseOut={hoverOut} value="left" className="btn">Left</button>
             <button onMouseOver={hoverIn} onMouseOut={hoverOut} value="right" className="btn">Right</button>
+            <div className={position} style={{display:displayTip? 'block':'none'}}>I am {position} tooltip</div>
         </div>
-        <div className={position} style={{display:displayTip? 'block':'none'}}>I am {position} tooltip</div>
+        <div>Please Hover on button</div>
     </div>
   )
 }
